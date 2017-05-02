9799c9e7ff69a1b3c812790fd2fa787452f631d8
JoelGaechter <joelgaechter@bluewin.ch>
2017-05-02 13:54:17 +0200

    DispEvent

---
+++ playgrounds/ivtExt/src/main/java/playground/clruch/net/DispEvent.java
@@ -0,0 +1,13 @@
+package playground.clruch.net;
+
+import playground.clruch.export.AVStatus;
+
+/**
+ * Created by Joel on 02.05.2017.
+ */
+public class DispEvent {
+    public AVStatus avStatus = null;
+    public int vehicleIndex = -1;
+    public int requestIndex = -1;
+    public int linkIndex = -1;
+}
